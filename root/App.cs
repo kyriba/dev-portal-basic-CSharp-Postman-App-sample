493ade3e50501806e34b0569a3c9356573fd018e
OlhaLevko <olha.levko@avenga.com>
2023-02-20 13:16:49 +0100

    updated with new postman version

---
+++ root/App.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text.Json;
+using RestSharp;
+
+namespace ConsoleApp1
+{
+    class App
+
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(PrettyJson(ExecuteRequest().Result));
+        }
+
+        private static string PrettyJson(string unPrettyJson)
+        {
+            var options = new JsonSerializerOptions(){
+                WriteIndented = true
+            };
+
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(unPrettyJson);
+
+            return JsonSerializer.Serialize(jsonElement, options);
+        }
+
+        private static async Task<string> ExecuteRequest()
+        {
+            //paste your code under this line
+            
+            
+            return response.Content;
+        }
+    }
+}
